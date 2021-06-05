764e94aca238435f8e390e66f3eef4949cb01149
Maksim <max.freedman.main@gmail.com>
2021-06-05 17:32:01 +0300

    changed ContextUtil class

--- src/main/java/base/spring/util/ContextUtil.java
+++ src/main/java/base/spring/util/ContextUtil.java
@@ -6,8 +6,16 @@ public class ContextUtil {
     private static final String CONFIGURATION_FILE = "applicationContext";
     private static final ClassPathXmlApplicationContext context = initContext();
 
+    private ContextUtil() {
+
+    }
+
     private static ClassPathXmlApplicationContext initContext() {
-        return new ClassPathXmlApplicationContext(CONFIGURATION_FILE);
+        try {
+            return new ClassPathXmlApplicationContext(CONFIGURATION_FILE);
+        } catch (Exception e) {
+            throw new RuntimeException("Error! Can't create ClassPathXmlApplicationContext object", e);
+        }
     }
 
     public static ClassPathXmlApplicationContext getContext() {
