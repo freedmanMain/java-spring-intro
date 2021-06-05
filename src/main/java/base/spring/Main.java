83f2206608fa217b6adfc5352d8df267e94e9dae
Maksim <max.freedman.main@gmail.com>
2021-06-05 12:25:13 +0300

    created ApplicationContext object in main method

--- src/main/java/base/spring/Main.java
+++ src/main/java/base/spring/Main.java
@@ -1,7 +1,14 @@
 package base.spring;
 
+import base.spring.model.Pet;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext");
+        Pet pet = context.getBean("dog", Pet.class);
+        pet.voice();
+        context.close();
     }
 }
