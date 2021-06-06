049e3ee03a65594e1662d35107efef6b7c305a51
Maksim <max.freedman.main@gmail.com>
2021-06-06 13:46:21 +0300

    completed ApplicationConfig class and main method

--- src/main/java/base/spring/Main.java
+++ src/main/java/base/spring/Main.java
@@ -1,9 +1,14 @@
 package base.spring;
 
+import base.spring.model.impl.PetOwner;
 import base.spring.util.AnnotationConfigApplicationContextUtil;
 
 public class Main {
     public static void main(String[] args) {
-
+        PetOwner petOwner = AnnotationConfigApplicationContextUtil
+                .getContext()
+                .getBean("petOwnerBean", PetOwner.class);
+        System.out.println(petOwner);
+        AnnotationConfigApplicationContextUtil.getContext().close();
     }
 }
--- src/main/java/base/spring/config/ApplicationConfig.java
+++ src/main/java/base/spring/config/ApplicationConfig.java
@@ -2,8 +2,10 @@ package base.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:app.properties")
 @ComponentScan("base.spring")
 public class ApplicationConfig {
 }
